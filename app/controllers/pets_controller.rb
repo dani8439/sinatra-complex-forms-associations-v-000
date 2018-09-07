4927c01fb54eb5de9fab1d844f0929d420d8240c
Dani Schuhman <dani8439@aol.com>
2018-09-07 23:20:39 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
 
-    if !params[:pet]
+    if !params[:pet].keys.include?("owner_id")
 
     redirect to "pets/#{@pet.id}"
   end
